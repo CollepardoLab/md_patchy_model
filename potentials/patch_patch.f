187e2a2fca249324edec9e9e8d822705b7f76d97
Jerelle Joseph <jaj52@login-e-12.data.cluster>
2019-05-29 09:45:06 +0100

    translated patch patch potential

--- potentials/patch_patch.f
+++ potentials/patch_patch.f
@@ -1,6 +1,6 @@
       dimension u(10000),rr(10000)
       dimension ucoulomb(10000),dericoulomb(10000)
-      dimension deri_analitico(10000),profundidad(20) 
+      dimension deri_analytical(10000),depth(20) 
       open(unit=1,file="table_0_00001kT.xvg",status="unknown")
       open(unit=2,file="table_0_02kT.xvg",status="unknown")
       open(unit=3,file="table_0_03kT.xvg",status="unknown")
@@ -23,40 +23,40 @@ c
       open(unit=88,file="profundidades_de_pozo.dat",status="unknown")
 c      
 c      open(1001,file="welldepth.dat")
-      anchura_pozo_sigma=0.12
-      anchura_interfase_sigma=0.005
+      width_well_sigma=0.12
+      width_interfase_sigma=0.005
       rgas=8.314
       temp=179.71
       rhog=0.000
-      profundidad(1)=0.00001
-      profundidad(2)=0.02
-      profundidad(3)=0.03
-      profundidad(4)=5.0
-      profundidad(5)=1.0
-      profundidad(6)=1.25
-      profundidad(7)=1.5
-      profundidad(8)=7.0
-      profundidad(9)=7.5
-      profundidad(10)=8.5
-      profundidad(11)=8.1
-      profundidad(12)=8.2
-      profundidad(13)=5.9
-      profundidad(14)=6.15
-      profundidad(15)=10.5
-      profundidad(16)=10.75
-      profundidad(17)=11.0
+      depth(1)=0.00001
+      depth(2)=0.02
+      depth(3)=0.03
+      depth(4)=5.0
+      depth(5)=1.0
+      depth(6)=1.25
+      depth(7)=1.5
+      depth(8)=7.0
+      depth(9)=7.5
+      depth(10)=8.5
+      depth(11)=8.1
+      depth(12)=8.2
+      depth(13)=5.9
+      depth(14)=6.15
+      depth(15)=10.5
+      depth(16)=10.75
+      depth(17)=11.0
 c
       npocillos=3054
 c 
       do it=1,17
-      profundidad_kT=profundidad(it) 
-c profundidad del pozo en kJ/mol
-      epsilon_pozo=profundidad_kT*rgas*temp/1000. 
-      rhol=epsilon_pozo
-c tamano del pocillo 0.35 sigma en nm 
-      z_0=anchura_pozo_sigma*3.405/10.
-c Anchura de la interfase en nm 
-      d=anchura_interfase_sigma*3.405/10.
+      depth_kT=depth(it) 
+c Depth of the well in kJ/mol
+      epsilon_well=depth_kT*rgas*temp/1000. 
+      rhol=epsilon_well
+c Well size 0.35 sigma in nm 
+      z_0=width_well_sigma*3.405/10.
+c Width of the interfase in nm 
+      d=width_interfase_sigma*3.405/10.
 c
       cero=0.000
 c
@@ -67,8 +67,8 @@ c
       dericoulomb(iz)=0.0
       auxi=(z-z_0)/d 
 
-c Para evitar NaN en la funcion hiperbolica
-c a distancias grandes 
+c To avoid NaN in the hyperbolic funcion 
+c at large distances  
       if (rr(iz).le.0.5) then
       senoh=(exp(auxi)-exp(-auxi))/2.
       coseh=(exp(auxi)+exp(-auxi))/2.
@@ -76,10 +76,10 @@ c a distancias grandes
       rho=0.5*(rhol+rhog)-0.5*(rhol-rhog)*tangenh
       u(iz)=-rho/4.184
       der_rho=-0.5*(rhol-rhog)/(coseh**2)/d
-      deri_analitico(iz)=-der_rho/4.184/10.0
+      deri_analytical(iz)=-der_rho/4.184/10.0
       else
       u(iz)=0.0000
-      deri_analitico(iz)=0.00000
+      deri_analytical(iz)=0.00000
       endif
 c
       enddo  
@@ -89,18 +89,18 @@ c
       write(it,*) "  "
       do iz=1,6000
       if (iz.eq.1) then 
-c      write(88,500) profundidad(it),-epsilon_pozo,u(iz),u(iz)*npocillos 
-      write(88,500) profundidad(it),u(iz),u(iz)*npocillos 
+c      write(88,500) depth(it),-epsilon_pozo,u(iz),u(iz)*npocillos 
+      write(88,500) depth(it),u(iz),u(iz)*npocillos 
       endif 
 c      deri=(u(iz+1)-u(iz-1))/(rr(iz+1)-rr(iz-1))
 c      fuerza=-deri
-      if (deri_analitico(iz) .lt. 0.1e-99) then 
-      deri_analitico(iz)=0.00000
-      write(it,200) iz,rr(iz)*10.0,u(iz),-deri_analitico(iz)
+      if (deri_analytical(iz) .lt. 0.1e-99) then 
+      deri_analytical(iz)=0.00000
+      write(it,200) iz,rr(iz)*10.0,u(iz),-deri_analytical(iz)
       else
-      write(it,200) iz,rr(iz)*10.0,u(iz),-deri_analitico(iz)
+      write(it,200) iz,rr(iz)*10.0,u(iz),-deri_analytical(iz)
       endif
-c      write(3,200) rr(iz),u(iz),-deri,-deri_analitico(iz) 
+c      write(3,200) rr(iz),u(iz),-deri,-deri_analytical(iz) 
       enddo
 c a por otro valor de kT 
       enddo 
